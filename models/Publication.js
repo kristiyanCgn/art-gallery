02e7b6f398d93cec36ec1f31f4a0b8be5a04b3be
kristiyanCgn <101466719+kristiyanCgn@users.noreply.github.com>
2022-10-27 19:57:16 +0200

    mongoose models added

---
+++ models/Publication.js
@@ -0,0 +1,43 @@
+const mongoose = require('mongoose');
+
+const URL_PATTERN = /^https?:\/\/(.+)/;
+
+const publicationSchema = new mongoose.Schema({
+    title: {
+        type: String,
+        required: true,
+        minlength: 6,
+    },
+    paintingTechnique: {
+        type: String,
+        required: true,
+        maxlength: 15,
+    },
+    artPicture: {
+        type: String,
+        required: true,
+        validate: {
+            validator(value) {
+                return URL_PATTERN.test(value);
+            },
+            message: 'Car image must be a valid URL!'
+        },
+    },
+    certificate: {
+        type: String,
+        enum: ['Yes', 'No'],
+        required: true,
+    },
+    author: {
+        type: mongoose.Types.ObjectId,
+        ref: 'User',
+    },
+    usersShared: [{
+        type: mongoose.Types.ObjectId,
+        ref: 'User',
+    }],
+});
+
+const Publication = mongoose.model('Publication', publicationSchema);
+
+module.exports = Publication;
---
+++ models/User.js
@@ -0,0 +1,42 @@
+const mongoose = require('mongoose');
+const bcrypt = require('bcrypt');
+const { SALT_ROUNDS } = require('../config/env');
+
+const userSchema = new mongoose.Schema({
+    username: {
+        type: String,
+        required: [true, 'Username is required!'],
+        minlength: 4,
+    },
+    password: {
+        type: String,
+        required: true,
+        minlength: 3,
+    },
+    address: {
+        type: String,
+        required: true,
+        maxlength: 20,
+    },
+    publications: [{
+        type: mongoose.Types.ObjectId,
+        ref: 'Publications',
+    }],
+    shares: [{
+        type: mongoose.Types.ObjectId,
+        ref: 'Publications',
+    }],
+});
+
+userSchema.pre('save', function(next){
+    bcrypt.hash(this.password, SALT_ROUNDS)
+        .then(hashedPassword => {
+            this.password = hashedPassword;
+
+            next();
+        });
+});
+
+const User = mongoose.model('User', userSchema);
+
+module.exports = User;
