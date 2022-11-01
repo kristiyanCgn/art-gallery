2c6edffa32656a0a69218a59e3232c81ddd62425
kristiyanCgn <101466719+kristiyanCgn@users.noreply.github.com>
2022-11-01 14:40:20 +0100

    home controller added

---
+++ controllers/homeController.js
@@ -0,0 +1,13 @@
+const router = require('express').Router();
+
+const publicationService = require('../services/publicationService');
+
+router.get('/', async (req, res) => {
+    const publicationResult = await publicationService.getAll().lean();
+
+    const publications = publicationResult.map(x => ({...x, shareCount: x.usersShared.length}));
+
+    res.render('home', { publications });
+});
+
+module.exports = router;
