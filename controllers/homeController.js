20c6e1f14e27de7b31bbe58aea27fde8267594b6
kristiyanCgn <101466719+kristiyanCgn@users.noreply.github.com>
2022-11-02 15:43:14 +0100

    profile route added

--- controllers/homeController.js
+++ controllers/homeController.js
@@ -1,6 +1,7 @@
 const router = require('express').Router();
 
 const publicationService = require('../services/publicationService');
+const userService = require('../services/userService');
 
 router.get('/', async (req, res) => {
     const publicationResult = await publicationService.getAll().lean();
@@ -8,6 +9,14 @@ router.get('/', async (req, res) => {
     const publications = publicationResult.map(x => ({...x, shareCount: x.usersShared.length}));
 
     res.render('home', { publications });
+});
+
+router.get('/profile', async (req, res) => {
+   const user = await userService.getOne(req.params._id).populate('publications').populate('shares').lean();
+   const publicationTitles = user.publications.map(x => x.title).join(', ');
+   const sharedTitles = user.shares.map(x => x.title).join(', ');
+
+   res.render('home/profile', { ...user, publicationTitles, sharedTitles });
 });
 
 module.exports = router;
